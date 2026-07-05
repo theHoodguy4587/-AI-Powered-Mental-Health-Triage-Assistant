006aa520242de3dc1167e141ea2e950f595a5bbb
Senitha Gunathilaka <senitha02@gmail.com>
2026-07-05 16:14:44 +0530

    eval

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import joblib
-
-from sklearn.metrics import classification_report, accuracy_score
-
+from sklearn.metrics import classification_report, accuracy_score,confusion_matrix,ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
 from preprocess import clean_text
 
 
@@ -11,6 +10,15 @@ val = pd.read_csv(
     "data/raw/val.csv"
 )
 
+risk_mapping = {
+    "self.offmychest": "Low Risk",
+    "self.Anxiety": "Moderate Risk",
+    "self.depression": "Moderate Risk",
+    "self.bipolar": "Moderate Risk",
+    "self.SuicideWatch": "High Risk"
+}
+
+val['risk_label'] = val['label'].map(risk_mapping)
 
 # Create clean_text column
 val['clean_text'] = val['text'].apply(clean_text)
@@ -33,12 +41,18 @@ X_val = tfidf.transform(
 
 
 # Labels
-y_val = val['label']
+y_val = val['risk_label']
 
 
 # Prediction
 pred = model.predict(X_val)
 
+cm = confusion_matrix(y_val,pred)
+
+disp = ConfusionMatrixDisplay(
+    confusion_matrix=cm,
+    display_labels=model.classes_
+)
 
 print(classification_report(
     y_val,
@@ -50,3 +64,7 @@ print(
     "Accuracy:",
     accuracy_score(y_val,pred)
 )
+
+disp.plot(cmap="Blues")
+plt.title("Confusion Matrix")
+plt.show()
